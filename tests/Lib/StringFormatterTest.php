5b621f3f583517fb79c398bb03f4ccee584b47c6
Samuel <samuelantunes@hotmail.fr>
2017-02-01 12:05:18 +0100

    #04DEV - TestUnit concatString Method

--- tests/Lib/StringFormatterTest.php
+++ tests/Lib/StringFormatterTest.php
@@ -17,4 +17,16 @@ class StringFormatterTest extends \PHPUnit_Framework_TestCase {
 		$result = $myClass->toCamelCase("my", "method"); 
 		$this->assertNotEquals($result, "mymethod");
 	}
+
+	public function testConcatStringSuccess() {
+		$myClass = new StringFormatter();
+		$result = $myClass->concatString("my", "method");
+		$this->assertEquals($result, "mymethod");
+	}
+
+	public function testConcatStringError() {
+		$myClass = new StringFormatter();
+		$result = $myClass->concatString("my", "method"); 
+		$this->assertNotEquals($result, "myMethod");
+	}
 }
