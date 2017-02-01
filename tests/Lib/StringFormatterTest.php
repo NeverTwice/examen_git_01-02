17f8729ade10cc5184a2c9a0633a557baf043ef5
Samuel <samuelantunes@hotmail.fr>
2017-02-01 12:00:54 +0100

    #03DEV - TestUnit toCamelCase Method

--- tests/Lib/StringFormatterTest.php
+++ tests/Lib/StringFormatterTest.php
@@ -2,8 +2,19 @@
 
 namespace tests\Lib;
 
-use Lib\StringFormatter.php;
+use Lib\StringFormatter;
 
 class StringFormatterTest extends \PHPUnit_Framework_TestCase {
 
+	public function testToCamelCaseSuccess() {
+		$myClass = new StringFormatter();
+		$result = $myClass->toCamelCase("my", "method");
+		$this->assertEquals($result, "myMethod");
+	}
+
+	public function testToCamelCaseError() {
+		$myClass = new StringFormatter();
+		$result = $myClass->toCamelCase("my", "method"); 
+		$this->assertNotEquals($result, "mymethod");
+	}
 }
